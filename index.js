a6e8108e81da28c5630a4ebdb6ce34eb5e892859
soheil <solyacount@gmail.com>
2025-10-04 23:58:58 +0330

    adding global variables

---
+++ index.js
@@ -0,0 +1,11 @@
+//--Global--
+const searchFormEl = document.querySelector('.search');
+const searchInputEl = document.querySelector('.search__input');
+const searchBtn = document.querySelector('.search__submit-btn');
+const searchResultNumberEl = document.querySelector('.count__number u-bold');
+const sortingIconEl = document.querySelector('.fa-solid fa-arrow-down-short-wide sorting__icon');
+const sortByRelevantBtn = document.querySelector('.sorting__button sorting__button--relevant sorting__button--active');
+const sortByRecentBtn = document.querySelector('.sorting__button sorting__button--recent');
+
+
+
