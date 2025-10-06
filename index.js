2cc13186abf9ae458ee8c15a8e968e6e794ed6f0
soheil <solyacount@gmail.com>
2025-10-06 16:02:38 +0330

    still working on base

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //--Global--
+const bookmarks_Btn = document.querySelector('.bookmarks-btn');
 const searchFormEl = document.querySelector('.search');
 const searchInputEl = document.querySelector('.search__input');
 const searchBtn = document.querySelector('.search__submit-btn');
@@ -6,6 +7,9 @@ const searchResultNumberEl = document.querySelector('.count__number u-bold');
 const sortingIconEl = document.querySelector('.fa-solid fa-arrow-down-short-wide sorting__icon');
 const sortByRelevantBtn = document.querySelector('.sorting__button sorting__button--relevant sorting__button--active');
 const sortByRecentBtn = document.querySelector('.sorting__button sorting__button--recent');
+const pagingBtn = document.querySelector('.pagination__button pagination__button--next');
+const jobsToShow = document.querySelector('.job-details__content');
+
 
 
 
