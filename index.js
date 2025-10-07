4ab9789043dba5068d60430a133234328290a91f
soheil <solyacount@gmail.com>
2025-10-08 00:02:04 +0330

    first parts of search bar submit handler

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //--Global--
+const errorEl = document.querySelector('.error');
 const bookmarks_Btn = document.querySelector('.bookmarks-btn');
 const searchFormEl = document.querySelector('.search');
 const searchInputEl = document.querySelector('.search__input');
@@ -10,6 +11,21 @@ const sortByRecentBtn = document.querySelector('.sorting__button sorting__button
 const pagingBtn = document.querySelector('.pagination__button pagination__button--next');
 const jobsToShow = document.querySelector('.job-details__content');
 
+//SEARCH COMPONENT
 
-
+function searchSubmitHandler (event){
+    event.preventDefault();
+    //get input search text
+    const searchText = searchInputEl.value;
+    const forbiddenPattern = /[0-9]/;
+    const PatternMatch = forbiddenPattern.test(searchText);
+    if(PatternMatch===true){
+        errorEl.classList.add('error--visible');
+        setTimeout(timer=>{
+            errorEl.classList.remove('error--visible');
+        },3000);
+    }
+    
+}
+searchFormEl.addEventListener('submit',searchSubmitHandler);
 
