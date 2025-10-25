cb10392572d58c98e69c74aa9bf6d28ece3258f8
soheil <solyacount@gmail.com>
2025-10-25 09:57:33 +0330

    finshed job Detail

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ const clickHandler = (event) =>{
     jobItemEl.classList.add('job-item--active');
     jobDetailsContentEl.innerHTML='';
     spinnerJobDetailsEl.classList.add('spinner--visible');
+    //fetch job item detail
     const jobItemId = jobItemEl.children[0].getAttribute('href');
     console.log(jobItemId);
     fetch(`https://bytegrad.com/course-assets/js/2/api/jobs/${jobItemId}`)
@@ -99,14 +100,70 @@ const clickHandler = (event) =>{
         return response.json();
     })
     .then(data =>{
-        console.log(data);
+        //show job Detail
+        const {jobItem}=data;
+        console.log(jobItem);
+        
+        spinnerJobDetailsEl.classList.remove('spinner--visible');
+        const jobDetail =`
+        <img src="${jobItem.coverImgURL}" alt="#" class="job-details__cover-img">
+
+<a class="apply-btn" href="https://fictionalversonetworkswebsite.com" target="_blank">Apply <i class="fa-solid fa-square-arrow-up-right apply-btn__icon"></i></a>
+
+<section class="job-info">
+    <div class="job-info__left">
+        <div class="job-info__badge">${jobItem.badgeLetters}</div>
+        <div class="job-info__below-badge">
+            <time class="job-info__time">${jobItem.daysAgo}d</time>
+            <button class="job-info__bookmark-btn">
+                <i class="fa-solid fa-bookmark job-info__bookmark-icon"></i>
+            </button>
+        </div>
+    </div>
+    <div class="job-info__right">
+        <h2 class="second-heading">${jobItem.title}</h2>
+        <p class="job-info__company">Verso Networks</p>
+        <p class="job-info__description">${jobItem.description}</p>
+        <div class="job-info__extras">
+            <p class="job-info__extra"><i class="fa-solid fa-clock job-info__extra-icon"></i> ${jobItem.duration}</p>
+            <p class="job-info__extra"><i class="fa-solid fa-money-bill job-info__extra-icon"></i> ${jobItem.salary}</p>
+            <p class="job-info__extra"><i class="fa-solid fa-location-dot job-info__extra-icon"></i> ${jobItem.location}</p>
+        </div>
+    </div>
+</section>
+
+<div class="job-details__other">
+    <section class="qualifications">
+        <div class="qualifications__left">
+            <h4 class="fourth-heading">Qualifications</h4>
+            <p class="qualifications__sub-text">Other qualifications may apply</p>
+        </div>
+        <ul class="qualifications__list">
+            <li class="qualifications__item">Node.js</li>
+        </ul>
+    </section>
+
+    <section class="reviews">
+        <div class="reviews__left">
+            <h4 class="fourth-heading">Company reviews</h4>
+            <p class="reviews__sub-text">Recent things people are saying</p>
+        </div>
+        <ul class="reviews__list">
+            <li class="reviews__item">Only job I liked going to.</li>
+        </ul>
+    </section>
+</div>
+
+<footer class="job-details__footer">
+    <p class="job-details__footer-text">If possible, please reference that you found the job on <span class="u-bold">rmtDev</span>, we would really appreciate it!</p>
+</footer>
+        `
+        jobDetailsContentEl.innerHTML=jobDetail;
+
     })
     .catch(err => console.log(err));
         
-    
-    
-    
-    
+      
 };
 
 jobListEl.addEventListener('click',clickHandler);
